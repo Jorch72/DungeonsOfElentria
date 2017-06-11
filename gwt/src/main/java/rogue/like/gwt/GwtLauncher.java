fe4cfbec72565a02efebcdc5f8ed3ec1f1f93054
Tommy Ettinger <tommy.ettinger@gmail.com>
2017-06-11 15:34:44 -0700

    Move existing code to earlier/ , make new project
    The Android project was causing problems for the other projects, so I
    removed it for now. The current project is just a blank screen skeleton
    project, but it should have the build files configured right.

---
+++ gwt/src/main/java/rogue/like/gwt/GwtLauncher.java
@@ -0,0 +1,20 @@
+package rogue.like.gwt;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import rogue.like.MainGame;
+
+/** Launches the GWT application. */
+public class GwtLauncher extends GwtApplication {
+    @Override
+    public GwtApplicationConfiguration getConfig() {
+        GwtApplicationConfiguration configuration = new GwtApplicationConfiguration(640, 480);
+        return configuration;
+    }
+
+    @Override
+    public ApplicationListener createApplicationListener() {
+        return new MainGame();
+    }
+}
